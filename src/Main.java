701f8de76c119f711074b82c4478cd37d4b00b21
Selvaperumal G <dev.iamselva@gmail.com>
2024-04-18 20:17:18 +0530

    Loop types conversion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(7);
+        String [] colours = {"red","yellow","blue"};
+        for (int i=0;i<colours.length;i++){
+            System.out.println(colours[i]);
+        }
+
+        String n = "47";
+        int num = Integer.parseInt(n);
+        System.out.println(num + 3);
+        float flt = (float) num;
+        System.out.println(flt);
+
+        int j = 65;
+        long l = j;
+        float f = l;
+
+        long lg = (long) f;
+        int in = (int) f;
+        char c = (char) in;
+        System.out.println(c);
+
     }
 }
