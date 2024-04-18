dbd75b15bc220def56cbf27ae441b97a194a1590
Selvaperumal G <dev.iamselva@gmail.com>
2024-04-18 20:15:45 +0530

    Askey Codes

---
+++ src/Char.java
@@ -0,0 +1,8 @@
+public class Char {
+    public static void main(String[] args) {
+        char c = 40;
+        for (int i = 0; i < 26; i++) {
+            System.out.println(c++);
+        }
+    }
+}
