72f89b10091f23ea24de3e916492cb545ffaeec0
Selvaperumal G <dev.iamselva@gmail.com>
2024-04-29 20:25:12 +0530

    Prime Number

---
+++ src/PrimeNumber.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter The Number :");
+        int number = Scanner.nextInt();
+
+        }
+    }
+
